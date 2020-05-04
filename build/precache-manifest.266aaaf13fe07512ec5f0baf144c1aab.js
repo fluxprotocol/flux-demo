53fb69f17a0e7efba5863d5942f0a2bad4187dae
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-05-04 13:38:14 +0200

    added build folder for pages test

---
+++ build/precache-manifest.266aaaf13fe07512ec5f0baf144c1aab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bd9af514c7226336daa8032a3ab158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "282d5f03d1af2baa5f5f",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "282d5f03d1af2baa5f5f",
+    "url": "/static/js/2.ce9eeeeb.chunk.js"
+  },
+  {
+    "revision": "262026c27ea312648c3f",
+    "url": "/static/js/main.d696faa7.chunk.js"
+  },
+  {
+    "revision": "927136db2e6c05b257b6",
+    "url": "/static/js/runtime-main.607127d9.js"
+  },
+  {
+    "revision": "636d6b5f7b228196bc270325befb638f",
+    "url": "/static/media/error.636d6b5f.png"
+  },
+  {
+    "revision": "fdcc1cfcb3a52c95c5d2b064e6f57726",
+    "url": "/static/media/filter-icon.fdcc1cfc.svg"
+  },
+  {
+    "revision": "0f9f85b8f4895d5c026470566b9b43c6",
+    "url": "/static/media/flux-logo.0f9f85b8.png"
+  },
+  {
+    "revision": "a3f4e3fd390da424c2286c86ee572e6e",
+    "url": "/static/media/iphone.a3f4e3fd.png"
+  },
+  {
+    "revision": "ca1d9d6cd89b1f579aaa70d6fa2ca8dc",
+    "url": "/static/media/success.ca1d9d6c.png"
+  }
+]);
